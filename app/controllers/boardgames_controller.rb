618145cf6a3f2e76249f5d96a5f0279efcd75f91
sophie <justanothersophie@gmail.com>
2021-09-10 01:51:14 -0400

    Routes and controller actions

--- app/controllers/boardgames_controller.rb
+++ app/controllers/boardgames_controller.rb
@@ -1,2 +1,10 @@
 class BoardgamesController < ApplicationController
+
+    def index
+        @boardgames = Boardgame.all
+    end
+
+    def show
+        @boardgame = Boardgame.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :boardgames
-  resources :genres
+  resources :boardgames, only: [:show, :index]
+  resources :genres, only: [:show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
